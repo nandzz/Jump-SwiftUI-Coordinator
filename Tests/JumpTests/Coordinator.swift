644b06c99a8139060fa96a8eb55cc27a14ae9515
Felipe Fernandes <nandz.it@gmail.com>
2023-02-26 20:23:02 +0100

    added more UnitTests
    removed unnecessary throws

--- Tests/JumpTests/Coordinator.swift
+++ Tests/JumpTests/Coordinator.swift
@@ -3,32 +3,30 @@ import XCTest
 @testable import Mocks
 
 class CoordinatorTests: XCTestCase {
-
+    
     var sut: TestCoordinatorMock!
-
-    override func setUpWithError() throws {
+    
+    override func setUp()  {
         sut = TestCoordinatorMock()
     }
-
-    override func tearDownWithError() throws {}
-
+    
     func test_root_is_loaded() throws {
         _ = sut.load(with: .root, navigation: true)
         XCTAssertEqual(sut.currentPath, .root)
     }
-
-    func test_three_views_are_presented() throws {
+    
+    func test_three_views_are_presented() {
         _ = sut.load(with: .root, navigation: true)
-
+        
         sut.onNext(current: .root)
         sut.onNext(current: .viewA)
-
+        
         XCTAssertEqual(sut.numberOfContextsPresented, 3)
     }
-
-    func test_currentContext_afterDismiss() throws {
+    
+    func test_currentContext_afterDismiss() {
         _ = sut.load(with: .root, navigation: true)
-
+        
         sut.onNext(current: .root)
         sut.onNext(current: .viewA)
         sut.onNext(current: .viewB)
@@ -37,10 +35,10 @@ class CoordinatorTests: XCTestCase {
             XCTAssertEqual(self.sut.currentPath, .viewC)
         }
     }
-
-    func test_show_root() throws {
+    
+    func test_show_root() {
         _ = sut.load(with: .root, navigation: true)
-
+        
         sut.onNext(current: .root)
         sut.onNext(current: .viewA)
         sut.onNext(current: .viewB)
@@ -50,9 +48,65 @@ class CoordinatorTests: XCTestCase {
         sut.onNext(current: .viewF)
         sut.onNext(current: .viewG)
         sut.onNext(current: .viewH)
-
+        
         sut.showRoot {
             XCTAssertEqual(self.sut.currentPath, .root)
+        }
+    }
+    
+    func test_dismiss_to() {
+        _ = sut.load(with: .root, navigation: true)
+        
+        sut.onNext(current: .root)
+        sut.onNext(current: .viewA)
+        sut.onNext(current: .viewB)
+        sut.onNext(current: .viewC)
+        sut.onNext(current: .viewD)
+        sut.onNext(current: .viewE)
+        sut.onNext(current: .viewF)
+        sut.onNext(current: .viewG)
+        sut.onNext(current: .viewH)
+        
+        sut.dismiss(to: .viewE) {
+            XCTAssertEqual(self.sut.currentPath, .viewE)
+        }
+    }
+    
+    func test_show_root_and_present_after_root_appears() {
+        _ = sut.load(with: .root, navigation: true)
+        
+        sut.onNext(current: .root)
+        sut.onNext(current: .viewA)
+        sut.onNext(current: .viewB)
+        sut.onNext(current: .viewC)
+        sut.onNext(current: .viewD)
+        sut.onNext(current: .viewE)
+        sut.onNext(current: .viewF)
+        sut.onNext(current: .viewG)
+        sut.onNext(current: .viewH)
+        
+        sut.showRoot() {
+            self.sut.present(.viewD, mode: .sheet)
+            XCTAssertEqual(self.sut.currentPath, .viewD)
+        }
+    }
+    
+    func test_dismiss_to_and_present_after_dismiss() {
+        _ = sut.load(with: .root, navigation: true)
+        
+        sut.onNext(current: .root)
+        sut.onNext(current: .viewA)
+        sut.onNext(current: .viewB)
+        sut.onNext(current: .viewC)
+        sut.onNext(current: .viewD)
+        sut.onNext(current: .viewE)
+        sut.onNext(current: .viewF)
+        sut.onNext(current: .viewG)
+        sut.onNext(current: .viewH)
+        
+        sut.dismiss(to: .viewE) {
+            self.sut.present(.viewB, mode: .sheet)
+            XCTAssertEqual(self.sut.currentPath, .viewB)
         }
     }
 }
